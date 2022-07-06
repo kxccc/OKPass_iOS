9434bccd37fef065bcda2e2bddef1fcd2b9ec550
czc <zckxccc@gmail.com>
2022-07-06 20:10:07 +0800

    Initial Commit

---
+++ OKPass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OKPass
+//
+//  Created by 陈治成 on 2022/7/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
