41cca92f344a4aa627c85c0d75f580cdfc333c05
czc <zckxccc@gmail.com>
2022-07-12 23:59:14 +0800

    部分完成生成密码

--- OKPass/GeneratePasswordView.swift
+++ OKPass/GeneratePasswordView.swift
@@ -22,12 +22,14 @@ protocol GeneratePasswordViewDelegate: AnyObject {
     private let passwordTextField: UITextField = .init()
     private let lenLabel: UILabel = .init()
     private let lenSlider: UISlider = .init()
-    private let upLetterLabel: UILabel = .init()
-    private let downLetterLabel: UILabel = .init()
-    private let digitalLabel: UILabel = .init()
-    private let upLetterSwitch: UISwitch = .init()
-    private let downLetterSwitch: UISwitch = .init()
-    private let digitalSwitch: UISwitch = .init()
+    private let uppercaseLabel: UILabel = .init()
+    private let lowercaseLabel: UILabel = .init()
+    private let numbersLabel: UILabel = .init()
+    private let symbolsLabel: UILabel = .init()
+    let uppercaseSwitch: UISwitch = .init()
+    let lowercaseSwitch: UISwitch = .init()
+    let numbersSwitch: UISwitch = .init()
+    let symbolsSwitch: UISwitch = .init()
     private let generateButton: UIButton = .init()
     private let confirmButton: UIButton = .init()
 
@@ -40,6 +42,35 @@ protocol GeneratePasswordViewDelegate: AnyObject {
         lenSlider.value = 0.5
         lenSlider.addTarget(self, action: #selector(lenSliderValueChanged(_:)), for: .valueChanged)
 
+        let uppercaseStack = UIStackView()
+        uppercaseLabel.text = "包含大写字母"
+        uppercaseStack.addArrangedSubview(uppercaseLabel)
+        uppercaseStack.addArrangedSubview(uppercaseSwitch)
+
+        let lowercaseStack = UIStackView()
+        lowercaseLabel.text = "包含小写字母"
+        lowercaseStack.addArrangedSubview(lowercaseLabel)
+        lowercaseStack.addArrangedSubview(lowercaseSwitch)
+
+        let numbersStack = UIStackView()
+        numbersLabel.text = "包含数字"
+        numbersStack.addArrangedSubview(numbersLabel)
+        numbersStack.addArrangedSubview(numbersSwitch)
+
+        let symbolsStack = UIStackView()
+        symbolsLabel.text = "包含特殊字符"
+        symbolsStack.addArrangedSubview(symbolsLabel)
+        symbolsStack.addArrangedSubview(symbolsSwitch)
+
+        let buttonStack = UIStackView()
+        buttonStack.spacing = 20
+        generateButton.configuration = .gray()
+        generateButton.setTitle("重新生成", for: .normal)
+        confirmButton.configuration = .filled()
+        confirmButton.setTitle("使用", for: .normal)
+        buttonStack.addArrangedSubview(generateButton)
+        buttonStack.addArrangedSubview(confirmButton)
+
         let vStack = UIStackView()
         vStack.translatesAutoresizingMaskIntoConstraints = false
         vStack.axis = .vertical
@@ -49,6 +80,11 @@ protocol GeneratePasswordViewDelegate: AnyObject {
         vStack.addArrangedSubview(passwordTextField)
         vStack.addArrangedSubview(lenLabel)
         vStack.addArrangedSubview(lenSlider)
+        vStack.addArrangedSubview(uppercaseStack)
+        vStack.addArrangedSubview(lowercaseStack)
+        vStack.addArrangedSubview(numbersStack)
+        vStack.addArrangedSubview(symbolsStack)
+        vStack.addArrangedSubview(buttonStack)
         addSubview(vStack)
 
         NSLayoutConstraint.activate([
@@ -59,6 +95,10 @@ protocol GeneratePasswordViewDelegate: AnyObject {
             passwordTextField.widthAnchor.constraint(equalTo: vStack.widthAnchor),
             lenLabel.widthAnchor.constraint(equalToConstant: 75),
             lenSlider.widthAnchor.constraint(equalTo: vStack.widthAnchor),
+            uppercaseStack.widthAnchor.constraint(equalToConstant: 170),
+            lowercaseStack.widthAnchor.constraint(equalToConstant: 170),
+            numbersStack.widthAnchor.constraint(equalToConstant: 170),
+            symbolsStack.widthAnchor.constraint(equalToConstant: 170),
         ])
     }
 
